638539ea435cd60244847092b77448d028a1234f
Morris Arroyo <arroyomorris@outlook.com>
2017-09-08 08:44:12 -0700

    added Student Class

---
+++ Student.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Description of Student
+ *
+ * @author Morris Arroyo
+ */
+class Student
+{
+    function __construct()
+    {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+
+    function add_email($which, $address) {
+        $this->emails[$which] = $address;
+    }
+
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+}
