141bfeb34104165604ba1a5c9a803df9eb817cd6
Morris Arroyo <arroyomorris@outlook.com>
2017-09-09 20:58:33 -0700

    added comments to Student.php

--- Student.php
+++ Student.php
@@ -3,10 +3,13 @@
 /**
  * Description of Student
  *
- * @author Morris Arroyo
+ * @author Jim, Morris Arroyo
  */
 class Student
 {
+    /*
+     * Constructs a student without any information.
+     */
     function __construct()
     {
         $this->surname = '';
@@ -15,14 +18,27 @@ class Student
         $this->grades = array();
     }
 
+    /*
+     * Adds an email to student's list of emails.
+     * @param string    $which    indicates email type, can be numbered
+     * @param string    $address  email address to be added
+     */
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
 
+    /*
+     * Adds a grade to student's list of grades.
+     * @param int  $grade  grade to be added
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
 
+    /*
+     * Calculates student's grade average.
+     * @return float  student's grade average
+     */
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -30,6 +46,10 @@ class Student
         return $total / count($this->grades);
     }
 
+    /*
+     * Returns student's information formatted for a browser.
+     * @return string  student's formatted information
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('. $this->average(). ")\n";
